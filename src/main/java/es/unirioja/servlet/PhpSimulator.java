bca4fb07f51cd4c305c59e16b51dad527f442b78
frpascua <francisco-jose.pascual@unirioja.es>
2023-04-25 12:27:03 +0200

    php url pattern

---
+++ src/main/java/es/unirioja/servlet/PhpSimulator.java
@@ -0,0 +1,20 @@
+package es.unirioja.servlet;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "PhpSimulator", urlPatterns = {"*.php"})
+public class PhpSimulator extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages/dummy.jsp");
+        rd.forward(request, response);
+    }
+
+}
